d2cb9b005735a913b64aef7498bc3e514a0f9ff6
Jesse Holwell <jesse.d.holwell@gmail.com>
2018-06-08 16:03:06 +1000

    Add project files.

---
+++ SoundReactions/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SoundReactions
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ SoundReactions/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SoundReactions.MvcApplication" Language="C#" %>
